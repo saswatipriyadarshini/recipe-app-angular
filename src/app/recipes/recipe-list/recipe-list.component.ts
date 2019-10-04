f144681532990fd8c040704b8ef20346e392a88f
Saswati Priyadarshini <spriyadarshini@crosslight.io>
2019-10-04 12:55:17 +0530

    added basic folder structure, navigation to recipes and shopping list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A Test Description', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg'),
+    new Recipe('A Test Recipe', 'A Test Description', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
